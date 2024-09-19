2fc3ae70799857c738ce6eabd8eb88045f6fc3f2
taehan0214 <kimtaehan0214@gmail.com>
2024-09-19 12:39:48 +0900

    feat:index 페이지 추가

--- src/app/index/page.tsx
+++ src/app/index/page.tsx
@@ -1,9 +1,10 @@
-import { Stack, Typography } from '@mui/material';
+import { Stack, Typography, } from '@mui/material';
 
 export default function IndexPage() {
   return (
     <Stack>
-      <Typography>Index Page</Typography>
+      <Typography>Index Page 입니다</Typography>
+      
     </Stack>
   );
 }
